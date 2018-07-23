65098c383fd64dff3f69c3a58f81d4260b5ae22e
qianyong.feng <qianyong.feng@woqutech.com>
2018-07-23 14:10:50 +0800

    add README

---
+++ README.md
@@ -0,0 +1,110 @@
+﻿[toc]
+
+
+
+---
+
+## 1. 要解决的问题
+
+1. ipmitool的命令这么长，记不住啊
+
+2. 使用IBM开发的pyghmi包封装，不需要使用者安装ipmitool工具
+
+## 2. 环境要求
+
+```
+
+pip install pyghmi
+
+pip install plumbum
+
+```
+
+## 3. 使用说明
+
+```bash
+
+ipmimgr 1.0.0
+
+
+
+Usage:
+
+    ipmimgr [SWITCHES]
+
+
+
+Meta-switches:
+
+    -h, --help Prints this help message and quits
+
+    --help-all Print help messages of all subcommands and quit
+
+    -v, --version Prints the program's version and quits
+
+
+
+Switches:
+
+    -g if given, get power status   # 不可与-s共用，获取节点电源状态
+
+    -i IP:str IPMI IP,example: 111(only 10.10.90.111)/10.10.xxx.xxx   # 节点的ipmi ip，90网段直接输入最后一段即可，其他网段使用全ip
+
+    -p PWD:str IPMI PASSWORD,default: 123456; requires -u   # ipmi pwd，默认为123456
+
+    -s CMD:{'reset', 'on', 'off'} set power on|off|reset; requires -i; excludes -g   # 改变电源状态，重启(reset)、开启(on)、关闭(off)
+
+    -u USER:str IPMI USER,default: ADMIN; requires -i, -p   # ipmi user，默认为ADMIN
+
+```
+
+## 4. Example
+
+#### 4.1 查看节点的电源状态
+
+>10.10.90.11的ipmi ip为10.10.90.111，账号ADMIN，密码123456
+
+```bash
+
+➜ ~ ipmimgr -g -i 111
+
+[ IPMIINFO ] ipmiip: 10.10.90.111, ipmiuser: ADMIN, ipmipwd: 123456
+
+[ ERROR ] failed: not connect
+
+```
+
+```bash
+
+➜ ~ ipmimgr -g -i 10.10.90.111
+
+[ IPMIINFO ] ipmiip: 10.10.90.111, ipmiuser: ADMIN, ipmipwd: 123456
+
+[ ERROR ] failed: not connect
+```
+
+```bash
+
+➜ ~ ipmimgr -g -i 10.10.90.111 -u ADMIN -p 123456
+
+[ IPMIINFO ] ipmiip: 10.10.90.111, ipmiuser: ADMIN, ipmipwd: 123456
+
+[ ERROR ] failed: not connect
+
+```
+
+#### 4.2. 修改电源状态
+
+>10.10.90.11的ipmi ip为10.10.90.111，账号ADMIN，密码123456
+
+## 5. TODO
+
+#### 5.1. 解决依赖
+
+#### 5.2. go重建
+
+## 6. FAQ
+
+发现远程执行ipmi查看/设置节点电源状态时，会出现大约30s超时现象，
+
+但是此时在同一qdata集群(同一网段？)的其他节点执行则不会有这种情况
---
+++ ipmimgr.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: 2.7.10
+@author: vonhng
+@contact: vonhng@qq.com
+@file: power.py
+@time: 2018/07/13 09:34
+"""
+from pyghmi.ipmi import command as ipmi_command
+from plumbum import cli, colors
+
+
+class Power(cli.Application):
+    VERSION = colors.green | "1.0.0"
+    COLOR_GROUPS = {"Meta-switches": colors.bold & colors.yellow, "Switches": colors.green}
+
+    _action, _bmc, _user, _pwd = ("", "", "ADMIN", "123456")
+
+    get_power = cli.Flag("-g", help="if given, get power status")
+
+    @cli.switch("-s", cli.Set("on", "off", "reset", case_sensitive=True), excludes=["-g"], requires=["-i"], help="set power on|off|reset")
+    def set_power(self, cmd):
+        self._action = cmd
+
+    @cli.switch("-i", str, help="IPMI IP,example: 111(only 10.10.90.111)/10.10.xxx.xxx")
+    def get_ip(self, ip):
+        if ip.isdigit():
+            bmc = "10.10.90.{}".format(int(ip))
+        else:
+            bmc = ip
+        self._bmc = bmc
+
+    @cli.switch("-p", str, requires=["-u"], help="IPMI PASSWORD,default: 123456")
+    def get_pwd(self, pwd):
+        self._pwd = pwd
+
+    @cli.switch("-u", str, help="IPMI USER,default: ADMIN", requires=["-i", "-p"])
+    def get_user(self, user):
+        self._user = user
+
+    def main(self):
+        print colors.bold | "[ IPMIINFO ] ipmiip: {}, ipmiuser: {}, ipmipwd: {}".format(self._bmc, self._user, self._pwd)
+        try:
+            ipmicmd = ipmi_command.Command(bmc=self._bmc, userid=self._user, password=self._pwd)
+        except Exception:
+            print colors.warn | "[ ERROR ] failed: not connect"
+            return
+        if self.get_power:
+            try:
+                ret = ipmicmd.get_power()
+            except Exception as e:
+                print colors.warn | "[ ERROR ] failed: {}".format(e)
+            else:
+                print colors.green | "[ OK ] power is {}".format(ret.get("powerstate", "unknow"))
+        else:
+            try:
+                ret = ipmicmd.set_power(self._action, wait=10)
+            except Exception as e:
+                print colors.warn | "[ ERROR ] failed: {}".format(e)
+            else:
+                print colors.green | "[ OK ] power is {}".format(ret.get("powerstate", "unknow"))
+
+
+if __name__ == "__main__":
+    Power.run()
